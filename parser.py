b38eb7c5bd5ff403d0d4ae9b20d74396e29bc4cb
Dan <zhuguotian1998@gmail.com>
2019-05-22 15:47:46 -0400

    Heatmap and misc

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 import csv
 
-filename = "TOF01.csv"
+filename = "data/data1.csv"
 
 title = [0,0,0]
 time = []
@@ -34,22 +34,28 @@ while i < len(latest):
 
 data = []
 
+print(min(single_dist), max(single_dist))
 x = 0
 y = 0
 
 for i in range(len(single_dist)):
+
 	if(x == 20):
 		x = 0
 	if(i%20 == 0):
 		y += 1
 	x += 1
+	#print([single_dist[i], x, y])
 	data.append([x, y, single_dist[i]])
+#print(data)
 
-with open("single_time_frame.csv", 'w') as csvfile:
-	csvwriter = csv.writer(csvfile)
-	csvwriter.writerow(title)
-	for i in range(len(data)):
-		csvwriter.writerow(data[i])
+# with open("single_time_frame", 'w') as csvfile:
+# 	csvwriter = csv.writer(csvfile)
+# 	csvwriter.writerow(title)
+# 	for i in range(len(data)):
+# 		csvwriter.writerow(data[i])
+
+
 
 
 
